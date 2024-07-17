5f7bf523f9c27a0ef93ffe7d7b1ad97a2cd66770
Wgz <1808365012@mail.ynu.edu.cn>
2024-07-17 16:53:59 +0800

    短链接分组模块-开发修改短链接分组功能

--- admin/src/main/java/org/wgz/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/org/wgz/shortlink/admin/controller/GroupController.java
@@ -1,13 +1,11 @@
 package org.wgz.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.wgz.shortlink.admin.common.convention.result.Result;
 import org.wgz.shortlink.admin.common.convention.result.Results;
 import org.wgz.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import org.wgz.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import org.wgz.shortlink.admin.dto.resp.ShortLinkGroupListRespDTO;
 import org.wgz.shortlink.admin.service.GroupService;
 
@@ -24,7 +22,7 @@ public class GroupController {
     private final GroupService groupService;
 
     /**
-     * 新增短链接
+     * 新增短链接分组
      */
     @PostMapping("/api/shortLink/v1/group")
     public Result<Void> saveGroup(@RequestBody ShortLinkGroupSaveReqDTO shortLinkGroupSaveReqDTO) {
@@ -38,5 +36,14 @@ public class GroupController {
     @GetMapping("/api/shortLink/v1/group")
     public Result<List<ShortLinkGroupListRespDTO>> shortLinkGroupList() {
         return Results.success(groupService.groupList());
+    }
+
+    /**
+     * 新增短链接分组
+     */
+    @PutMapping("/api/shortLink/v1/group")
+    public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO shortLinkGroupUpdateReqDTO) {
+        groupService.updateGroup(shortLinkGroupUpdateReqDTO);
+        return Results.success();
     }
 }
