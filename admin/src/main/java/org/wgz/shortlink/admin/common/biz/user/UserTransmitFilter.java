4f26aa21bfe69ac057bb1a5b6cc365d0b048f540
Wgz <1808365012@mail.ynu.edu.cn>
2024-08-02 15:19:23 +0800

    优化拦截用户上下文功能

--- admin/src/main/java/org/wgz/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/wgz/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,15 +1,19 @@
 package org.wgz.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.wgz.shortlink.admin.common.convention.exception.ClientException;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+
+import static org.wgz.shortlink.admin.common.enums.UserErrorCodeEnums.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -18,8 +22,8 @@ import java.util.Objects;
 public class UserTransmitFilter implements Filter {
 
     private static final List<String> IGNORE_URL = Lists.newArrayList(
-            "/api/shortLink/v1/user/login"
-            ,"/api/shortLink/v1/user/has-username");
+            "/api/shortLink/admin/v1/user/login"
+            , "/api/shortLink/admin/v1/user/has-username");
 
     private final StringRedisTemplate stringRedisTemplate;
 
@@ -27,21 +31,34 @@ public class UserTransmitFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURL = httpServletRequest.getRequestURI();
-        if(!IGNORE_URL.contains(requestURL)){
+        if (!IGNORE_URL.contains(requestURL)) {
             String method = httpServletRequest.getMethod();
-            if(!Objects.equals(requestURL,"/api/shortLink/v1/user")){
+            if (!(Objects.equals(requestURL, "/api/shortLink/v1/user") && Objects.equals(method, "POST"))) {
                 String username = httpServletRequest.getHeader("username");
                 String token = httpServletRequest.getHeader("token");
-                Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-                if(userInfoJsonStr != null){
-                    UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(),UserInfoDTO.class);
-                    UserContext.setUser(userInfoDTO);
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    // TODO 网关响应前端
+                    return;
                 }
+                Object userInfoJsonStr;
                 try {
-                    filterChain.doFilter(servletRequest, servletResponse);
-                } finally {
-                    UserContext.removeUser();
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        // TODO 网关响应前端
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                } catch (Exception e) {
+                    // TODO 网关响应前端
+                    throw new ClientException(USER_TOKEN_FAIL);
                 }
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+
+            }
+            try {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } finally {
+                UserContext.removeUser();
             }
         }
 
--- admin/src/main/java/org/wgz/shortlink/admin/common/enums/UserErrorCodeEnums.java
+++ admin/src/main/java/org/wgz/shortlink/admin/common/enums/UserErrorCodeEnums.java
@@ -4,6 +4,7 @@ import org.wgz.shortlink.admin.common.convention.errorCode.IErrorCode;
 
 public enum UserErrorCodeEnums implements IErrorCode {
 
+    USER_TOKEN_FAIL("A000200","用户Token验证失败"),
     USER_NULL("B000200", "用户记录不存在"),
     USER_NAME_EXIST("B000201", "用户名已存在"),
     USER_EXIST("B000202", "用户记录已存在"),
