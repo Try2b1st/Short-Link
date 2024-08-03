23302a1a8322a92bfffafc98198b59a1f04cf889
Wgz <1808365012@mail.ynu.edu.cn>
2024-08-03 11:52:53 +0800

    短链接分组数据库分表处理

--- admin/src/test/java/org/wgz/shortlink/admin/UserTableTest.java
+++ admin/src/test/java/org/wgz/shortlink/admin/UserTableTest.java
@@ -1,26 +1,18 @@
 package org.wgz.shortlink.admin;
 
 public class UserTableTest {
-    private static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+    private static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
             "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
-            "  `short_uri` varchar(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '短链接',\n" +
-            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
-            "  `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
-            "  `click_num` int(11) DEFAULT '0' COMMENT '点击量',\n" +
             "  `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
-            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
-            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
-            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
-            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
-            "  `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
+            "  `name` varchar(64) DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int(3) DEFAULT NULL COMMENT '分组排序',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  `favicon` varchar(256) DEFAULT NULL,\n" +
+            "  `del_flag` tinyint(1) DEFAULT '0' COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4\n" +
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;\n" +
             "\n";
 
     public static void main(String[] args) {
