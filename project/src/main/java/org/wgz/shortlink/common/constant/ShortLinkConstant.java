7dd0983a71cfbbda646e99761c9e8a8ef134f0ba
Wgz <1808365012@mail.ynu.edu.cn>
2024-08-04 15:00:34 +0800

    短链接创建进行缓存预热

--- project/src/main/java/org/wgz/shortlink/common/constant/ShortLinkConstant.java
+++ project/src/main/java/org/wgz/shortlink/common/constant/ShortLinkConstant.java
@@ -2,5 +2,8 @@ package org.wgz.shortlink.common.constant;
 
 public class ShortLinkConstant {
 
+    /**
+     * 永久有效期短链接默认 预热一个月
+     */
     public static final long DEFAULT_CACHE_VALID_TIME = 2626560000L;
 }
